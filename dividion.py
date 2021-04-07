431d8124f7643b625d5a3558ed7b801cfd5b55c7
Akhil dubey <dubeytech7024@gmail.com>
2021-04-07 17:08:50 +0530

    in dev area in

--- dividion.py
+++ dividion.py
@@ -1,6 +1,6 @@
 n=input("enter the first number \n ")
 m=input("enter the second number \n")
 if m==0:
-    print(" !!!!!!  Invalid Operators  !!!!!")
+    print(" !!!!!! hola hola !!!!!")
 else:
     print("quoteint %d", n/m)
